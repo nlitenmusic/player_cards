a547cef3152c891ab0d70de0604fa9d4126d8590
Jordan Tolbert <info.nlitenmusic@gmail.com>
2025-12-20 12:10:52 -0500

    chore(next): remove turbopack.root overrides from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,2 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Ensure Turbopack treats the project folder as the workspace root
-  turbopack: {
-    root: '.'
-  }
-};
-
-module.exports = nextConfig;
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // Ensure Turbopack/Next uses the subfolder containing the app as the workspace root
-  turbopack: {
-    root: 'products/player-cards-next'
-  }
-};
-
-module.exports = nextConfig;
+module.exports = {};
